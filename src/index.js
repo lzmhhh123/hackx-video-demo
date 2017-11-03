3318632eed54f7df7e90c3ff619334f752330fc3
lzmhhh123 <lzmhhh123@gmail.com>
2017-11-04 07:47:05 +0800

    to add api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
